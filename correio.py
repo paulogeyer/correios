a4458bbc993bb7a87c7bce2e6a4c3fe6836eb5ad
Paulo Geyer <paulogeyer@gmail.com>
2013-05-19 03:27:34 -0300

    meh

--- correio.py
+++ correio.py
@@ -21,9 +21,3 @@ def parse(page):
             entry["activity"] = line.contents[0].get_text()
         history.append(entry)
     return history
-
-if len(sys.argv) > 1:
-    page = urllib2.urlopen("http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI="+sys.argv[1])
-    for item in parse(page.read()):
-        print item["datetime"]
-        print item["local"],":",item["activity"]

65974cb3bb863e4c906ee3066c70124c15e0609d
Paulo Geyer <paulogeyer@gmail.com>
2014-07-04 00:44:52 -0300

    atualizando parser para novo formato da página dos correios

--- correio.py
+++ correio.py
@@ -4,20 +4,18 @@ import urllib2
 from bs4 import BeautifulSoup
 
 def parse(page):
-    page = page.replace('\n','').replace('\r','')
-    doc = BeautifulSoup(re.match('.*entrega\.<p>(.+)<hr.*', page).group(1), from_encoding="ISO-8859-1")
-    lines = list(reversed(doc.select('tr')))[:-1]
     history = []
+    page = page.replace('\n','').replace('\r','')
+    doc = BeautifulSoup(page, from_encoding="ISO-8859-1")
+    lines = doc.select('table.listEvent.sro tr')
     for line in lines:
-        line_num = len(line.contents)
-        entry = {}
-        if line_num == 3:
-            entry["datetime"] = line.contents[0].get_text().encode('latin-1')
-            entry["local"] = line.contents[1].get_text().encode('latin-1')
-            entry["activity"] = line.contents[2].get_text().encode('latin-1')
-        elif line_num == 1:
-            entry["datetime"] = history[-1]["datetime"]
-            entry["local"] = history[-1]["local"]
-            entry["activity"] = line.contents[0].get_text()
-        history.append(entry)
+	entry = {}
+	date, time, local, _ = ' '.join(line.contents[0].get_text().encode('latin-1').split()).split()
+	entry["datetime"] = date+" "+time
+	entry["local"] = local
+	entry["activity"] = line.contents[1].get_text().encode('latin-1')
+	history.append(entry)
     return history
+
+
+	
